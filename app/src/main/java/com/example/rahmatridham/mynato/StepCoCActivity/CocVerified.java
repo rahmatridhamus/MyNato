49f2e7e95f529f2327e4687da8f60ede1041da88
rahmatridhamus <rahmatridham@hotmail.com>
2017-02-13 01:24:23 +0700

    fix User Interface

--- app/src/main/java/com/example/rahmatridham/mynato/StepCoCActivity/CocVerified.java
+++ app/src/main/java/com/example/rahmatridham/mynato/StepCoCActivity/CocVerified.java
@@ -5,44 +5,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.rahmatridham.mynato.R;
 
 public class CocVerified extends AppCompatActivity {
+    Button next;
+    RadioGroup group;
 
-    TextView kontenCOChari,itemRapat;
-    CardView adminInput,AnggotaCoc;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_coc_senin);
+        setContentView(R.layout.activity_coc_verified);
 
-        kontenCOChari = (TextView) findViewById(R.id.kontenCocHari);
-//        itemRapat = (TextView) findViewById(R.id.kontenCocHari);
-        adminInput = (CardView) findViewById(R.id.adminInput);
-        AnggotaCoc = (CardView) findViewById(R.id.anggotaCoc);
+        next = (Button) findViewById(R.id.buttonSelanjutnya);
+        group = (RadioGroup) findViewById(R.id.radioTipecoc);
 
-        Intent iin= getIntent();
-        Bundle b = iin.getExtras();
-        if(b!=null)
-        {
-            String day =(String) b.get("menuClicked");
-            kontenCOChari.setText("Konten CoC "+day);
-        }
-
-        adminInput.setOnClickListener(new View.OnClickListener() {
+        next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(CocVerified.this, Step1VisiMisi.class));
-            }
-        });
-
-        AnggotaCoc.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(CocVerified.this, "testing", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(CocVerified.this,PrepareAddCoc.class);
+                startActivity(intent);
             }
         });
     }
---
+++ app/src/main/java/com/example/rahmatridham/mynato/StepCoCActivity/PrepareAddCoc.java
@@ -0,0 +1,48 @@
+package com.example.rahmatridham.mynato.StepCoCActivity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.rahmatridham.mynato.R;
+
+public class PrepareAddCoc extends AppCompatActivity {
+    TextView kontenCOChari,itemRapat;
+    CardView adminInput,AnggotaCoc;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prepare_add_coc);
+
+        kontenCOChari = (TextView) findViewById(R.id.kontenCocHari);
+//        itemRapat = (TextView) findViewById(R.id.kontenCocHari);
+        adminInput = (CardView) findViewById(R.id.adminInput);
+        AnggotaCoc = (CardView) findViewById(R.id.anggotaCoc);
+
+        Intent iin= getIntent();
+        Bundle b = iin.getExtras();
+        if(b!=null)
+        {
+            String day =(String) b.get("menuClicked");
+            kontenCOChari.setText("Konten CoC "+day);
+        }
+
+        adminInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PrepareAddCoc.this, Step1VisiMisi.class));
+            }
+        });
+
+        AnggotaCoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(PrepareAddCoc.this, "testing", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
